cae90d6e4ea220a0688c7ad56fceade17978a016
Irina <balkarovairina@gmail.com>
2022-04-22 12:05:51 +0200

    Add ROOM-database setup

---
+++ todoapp/app/src/main/java/ru/irinavb/todoapp/data/ToDoDatabase.kt
@@ -0,0 +1,10 @@
+package ru.irinavb.todoapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.irinavb.todoapp.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+}
