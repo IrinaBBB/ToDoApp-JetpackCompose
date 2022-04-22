0f47c6279504a43dccdeeef4829ce2a0689e5d4f
Irina <balkarovairina@gmail.com>
2022-04-22 12:47:47 +0200

    Add ToDoRepository

--- todoapp/app/src/main/java/ru/irinavb/todoapp/MainActivity.kt
+++ todoapp/app/src/main/java/ru/irinavb/todoapp/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import ru.irinavb.todoapp.ui.theme.ToDoAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ todoapp/app/src/main/java/ru/irinavb/todoapp/data/repositories/ToDoRepository.kt
@@ -0,0 +1,28 @@
+package ru.irinavb.todoapp.data.repositories
+
+import kotlinx.coroutines.flow.Flow
+import ru.irinavb.todoapp.data.ToDoDao
+import ru.irinavb.todoapp.data.models.ToDoTask
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> = toDoDao.getSelectedTask(id = taskId)
+
+    fun searchDatabase(searchQuery:String): Flow<List<ToDoTask>> =
+        toDoDao.searchDatabase(searchQuery = searchQuery)
+
+    suspend fun addTask(toDoTask: ToDoTask) = toDoDao.addTask(toDoTask = toDoTask)
+
+    suspend fun updateTask(toDoTask: ToDoTask) = toDoDao.updateTask(toDoTask = toDoTask)
+
+    suspend fun deleteTask(toDoTask: ToDoTask) = toDoDao.deleteTask(toDoTask = toDoTask)
+
+    suspend fun deleteAllTasks() = toDoDao.deleteAllTasks()
+}
